fbc20a99e32f421d8b339114c05a4f03d9555eab
Dharit Parmar <dparmar91@gmail.com>
2018-07-11 16:11:51 -0400

    Fetches data with http service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {ProductProvider} from '../../providers/product/product';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: Http, public navCtrl: NavController, private productProvider: ProductProvider) {
 
   }
 
+  ionViewDidLoad(){
+    this.productProvider.getProduct()
+    .subscribe(response => console.log(response));
+  }
 }
