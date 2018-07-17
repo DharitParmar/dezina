d1d294ab2dac7fd804ec5cd0bdfa8dbcf9462a6e
Dharit Parmar <dparmar91@gmail.com>
2018-07-17 19:14:25 -0400

    Integrates the card component for best seller

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ProductProvider} from '../../providers/product/product';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public allBestSellerProducts = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private productProvider: ProductProvider) {
 
   }
 
+  ionViewDidLoad(){
+    this.productProvider.getProduct()
+      .subscribe((allProducts) => {
+        this.allBestSellerProducts = allProducts.filter(product => product.bestSeller == true);
+        // console.log(this.allBestSellerProducts);
+      })
+  }
 }
